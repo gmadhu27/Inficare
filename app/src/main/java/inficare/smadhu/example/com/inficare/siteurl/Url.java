210ea9d3140ea6b594d19af3d21b81b73de5634f
madhu Gautam <gmadhu27@gmail.com>
2018-05-31 15:11:24 +0545

    add url

---
+++ app/src/main/java/inficare/smadhu/example/com/inficare/siteurl/Url.java
@@ -0,0 +1,11 @@
+package inficare.smadhu.example.com.inficare.siteurl;
+
+/**
+ * Created by smadhu on 5/31/2018.
+ */
+
+public class Url {
+
+    public static final String packageName = "https://gateway.sandbox.npay.com.np/websrv/service.asmx";
+
+}
